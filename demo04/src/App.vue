235f5cd63ee09f77c142411f4ec440a69220ab96
Razzh <136013779@qq.com>
2022-05-16 15:39:55 +0800

    chore:基于Webpack下搭建的选项API功能测试

--- demo04/src/App.vue
+++ demo04/src/App.vue
@@ -2,12 +2,33 @@
 <div>
   <h1 class="orange">webpack手动搭建Vue2.x开发环境</h1>
   <h3 class="green">success!</h3>
+  <button @click="testEs6" :style="style">ES6语法</button>
 </div>
 </template>
 
 <script>
 export default {
-  
+  data () {
+    return {
+      obj: {
+        name: 'tsuzaki',
+        age: 18
+      }
+    }
+  },
+  methods: {
+    testEs6() {
+      const res = () => {
+        console.log('Es6语法实践')
+      }
+      res()
+    }
+  },
+  computed: {
+    style() {
+      return 'color: #340E9E;font-size: 22px;'
+    }
+  }
 }
 </script>
 
