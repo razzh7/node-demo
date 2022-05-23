d1bae295419ec3e617bf6fb17cc435465a75bf5f
Razzh <136013779@qq.com>
2022-05-23 16:30:37 +0800

    chore:devtool

--- demo04/src/App.vue
+++ demo04/src/App.vue
@@ -1,35 +1,36 @@
 <template>
-<div>
-  <h1 class="orange">webpack手动搭建Vue2.x开发环境</h1>
-  <h3 class="green">success!</h3>
-  <button @click="testEs6" :style="style">ES6语法</button>
-</div>
+  <div>
+    <h1 class="orange">webpack手动搭建Vue2.x开发环境</h1>
+    <h3 class="green">success!</h3>
+    <button @click="testEs6" :style="style">ES6语法</button>
+  </div>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       obj: {
-        name: 'tsuzaki',
-        age: 18
-      }
-    }
+        name: "tsuzaki",
+        age: 18,
+      },
+    };
   },
   methods: {
     testEs6() {
       const res = () => {
-        console.log('Es6语法实践')
-      }
-      res()
-    }
+        console.log("Es6语法实践");
+        console.log("this", test);
+      };
+      res();
+    },
   },
   computed: {
     style() {
-      return 'color: #340E9E;font-size: 22px;'
-    }
-  }
-}
+      return "color: #340E9E;font-size: 22px;";
+    },
+  },
+};
 </script>
 
 <style>
@@ -38,6 +39,6 @@ export default {
   color: orange;
 }
 .green {
-  color: #28E31E;
+  color: #28e31e;
 }
 </style>
